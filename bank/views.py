c1aebf736b366eb38b17a118f36575e68f62c295
Harsha8691 <163673098+Harsha8691@users.noreply.github.com>
2024-05-22 22:25:20 +0530

    Update views.py

--- bank/views.py
+++ bank/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Bank, Branch
+from .serializers import BankSerializer, BranchSerializer
 
-# Create your views here.
+class BankListView(generics.ListAPIView):
+    queryset = Bank.objects.all()
+    serializer_class = BankSerializer
+
+class BranchDetailView(generics.ListAPIView):
+    serializer_class = BranchSerializer
+
+    def get_queryset(self):
+        branch_name = self.kwargs['branch_name']
+        return Branch.objects.filter(name__icontains=branch_name)
