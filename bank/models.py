cf5bb8dbdb8d491a978e012197e60ca012493dc2
Harsha8691 <163673098+Harsha8691@users.noreply.github.com>
2024-05-22 22:42:54 +0530

    Update models.py

--- bank/models.py
+++ bank/models.py
@@ -1,18 +1,11 @@
+
 from django.db import models
 
 class Bank(models.Model):
-    name = models.CharField(max_length=100)
-    established_date = models.DateField()
-
-    def __str__(self):
-        return self.name
+    name = models.CharField(max_length=255)
 
 class Branch(models.Model):
     bank = models.ForeignKey(Bank, related_name='branches', on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=200)
-    city = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
 
